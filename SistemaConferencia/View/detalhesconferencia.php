5eaa0762b8f5e34488739235b57d7305810416a5
IpebralServidor <servidorvirtualipebral@gmail.com>
2024-04-30 11:22:52 -0300

    commit alterações na etiqueta
    commit alterações na etiqueta

--- SistemaConferencia/View/detalhesconferencia.php
+++ SistemaConferencia/View/detalhesconferencia.php
@@ -309,18 +309,6 @@ while ($row2 = sqlsrv_fetch_array($stmt5, SQLSRV_FETCH_NUMERIC)) {
 			<div style="background-color: #ADADC7" class="d-flex justify-content-around">
 				<div>
 					<h4 style="margin: 0 !important; ">Itens em Conferência
-						<?php // if ($usuconf == 32) { 
-						?>
-						<button style="font-size: 13px;" id="gerarEtiqueta" onclick="impressao('etiqueta');">Imprimir etiqueta</button>
-						<?php //} 
-						?>
-
-						<?php //if ($usuconf == 32) { 
-						?>
-						<button style="font-size: 13px;" id="gerarVale" onclick="impressao('vale');">Imprimir vale</button>
-						<?php //} 
-						?>
-
 						<button style="font-size: 13px;" onclick="confirmar_conf();">Finalizar Conferência</button>
 						<button style="font-size: 13px;" onclick="abrirdivergencias();">Produtos Divergentes</button>
 						<?php
@@ -692,8 +680,6 @@ while ($row2 = sqlsrv_fetch_array($stmt5, SQLSRV_FETCH_NUMERIC)) {
 			</div>
 		</div> <!-- Itens do Pedido -->
 	</div> <!--container-->
-
-	<script src="../Controller/impressao.js"></script>
 
 	<script>
 		var index,
