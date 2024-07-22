de40829f3c9b36e3324a41939958f119bce6a4c6
IpebralServidor <servidorvirtualipebral@gmail.com>
2024-07-22 09:01:51 -0300

    Alterações no menu inicial do SistemaTransferencias
    - Aumento da área visível em celulares
    - Rearranjo das informações sobre as notas em andamento

--- SistemaTransferencias/Model/notasAndamento.php
+++ SistemaTransferencias/Model/notasAndamento.php
@@ -5,11 +5,13 @@ require_once '../../App/auth.php';
 
 $tsql = "SELECT 
             TGFCAB.NUNOTA, 
-            CODTIPOPER, 
-            (SELECT COUNT(1) FROM TGFITE WHERE TGFITE.NUNOTA = TGFCAB.NUNOTA AND SEQUENCIA > 0) AS QTD_ITENS,
             DTNEG,
-            CODUSU
+            CONCAT(USU.CODUSU, ' - ', NOMEUSU) AS USU,
+            (SELECT COUNT(1) FROM TGFITE WHERE TGFITE.NUNOTA = TGFCAB.NUNOTA AND SEQUENCIA > 0) AS QTD_ITENS,
+            TGFCAB.CODEMP
         FROM TGFCAB 
+        INNER JOIN TSIUSU USU
+        ON TGFCAB.CODUSU = USU.CODUSU
         WHERE CODTIPOPER = 1300 
           AND AD_PEDIDOECOMMERCE = 'TRANSF_NOTA_TRANSFERENCIAS'
           AND STATUSNOTA <> 'L'
@@ -19,9 +21,9 @@ $stmt = sqlsrv_query($conn, $tsql);
 while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
   echo '<tr onClick="abrirNota(this)" id="nunotaLinha" data-id="' . $row['NUNOTA'] . '">';
   echo '<td>' . $row['NUNOTA'] . '</td>';
-  echo '<td>' . $row['CODTIPOPER'] . '</td>';
-  echo '<td>' . $row['QTD_ITENS'] . '</td>';
   echo '<td>' . date_format($row['DTNEG'], "d/m/Y") . '</td>';
-  echo '<td>' . $row['CODUSU'] . '</td>';
+  echo '<td>' . $row['USU'] . '</td>';
+  echo '<td>' . $row['QTD_ITENS'] . '</td>';
+  echo '<td>' . $row['CODEMP'] . '</td>';
   echo '</tr>';
 }
