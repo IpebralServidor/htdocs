13cfa2115804cf592dd79d95306097873393c13a
leandro5760 <servidorvirtualipebral@gmail.com>
2025-03-27 13:49:33 -0300

    Atualizar Buscar Separador para Remedcar
    Remedcar agora conseguirá usar o buscar separador, para passar pela conferência criada, assim como funciona com o Balcão.

--- SistemaConferencia/Model/buscaseparador.php
+++ SistemaConferencia/Model/buscaseparador.php
@@ -6,14 +6,17 @@ $nunota = $_POST['nota'];
 
 $params = array($nunota);
 
-$tsql = "SELECT AD_SEPARADOR, CODTIPOPER, CODPARCTRANSP FROM TGFCAB WHERE NUNOTA = ?";
+$tsql = "SELECT AD_SEPARADOR, CODTIPOPER, CODPARCTRANSP, CODEMPPREF 
+         FROM TGFCAB INNER JOIN
+              TGFPAR ON TGFPAR.CODPARC = TGFCAB.CODPARC
+         WHERE NUNOTA = ?";
 $stmt = sqlsrv_query($conn, $tsql, $params);
 
 $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_NUMERIC);
 
 // CODPARCTRANSP 12092 é Apanha Balcão, para pedidos que serão retirados no balcão
 $tops = array(1721, 1722);
-if ($row[0] === NULL && ($row[1] == 1720 || (in_array($row[1], $tops) && $row[2] == 12092))) {
+if ($row[0] === NULL && ($row[1] == 1720 || (in_array($row[1], $tops) && $row[2] == 12092) || $row[3] == 5 || $row[3] == 30)) {
     echo $row[0];
 } else {
     echo 'ok';
